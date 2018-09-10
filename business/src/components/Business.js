3398e2bb53917b8a4ea3e2da0182164e54bc99ec
Ruth <github email address>
2018-09-10 11:41:04 -0400

    expand Business Component to include business details

---
+++ business/src/components/Business.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Business extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Consult Pearson EnterPrises</h1>
+        <button className="col.25">Create a Room</button>
+        <button className="col.25">Join a Room</button>
+      </div>
+    );
+  }
+};
+
+export default Business;
