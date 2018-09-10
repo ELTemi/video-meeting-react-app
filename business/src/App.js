d8dae5f27e63b2e2798c4dd7f00a243393e40978
Ruth <github email address>
2018-09-10 11:40:25 -0400

    import VideoCall and Business components

--- business/src/App.js
+++ business/src/App.js
@@ -3,35 +3,29 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import Business from './components/Business';
 import CallLogs from './components/CallLogs';
 import Contacts from './components/Contacts';
-import Call from './components/Call';
+import VideoCall from './components/VideoCall';
+import Business from './components/Business';
+
 
 class App extends Component {
-  render() {
+  render(props) {
     return (
-      <Router>
-        <div className="app">
-          <NavBar />
-          <Route exact path="/" component={Business} />
-          <Route exact path="/all_logs" component={CallLogs} />
-          <Route exact path="/contacts" component={Contacts} />
-          <Route exact path="/call" component={Call} />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Router>
+            <div className="app">
+              <NavBar />
+              <Route exact path="/" component={Business} />
+              <Route exact path="/all_logs" component={CallLogs} />
+              <Route exact path="/contacts" component={Contacts} />
+              <Route exact path="/call" component={VideoCall} />
+            </div>
+          </Router>
         </div>
-      </Router>
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-
-
     );
   }
 }
