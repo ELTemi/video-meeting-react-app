56c3a05836b343332688cf2cf7fec59406046356
Ruth <github email address>
2018-09-10 11:46:20 -0400

    add CallContainer

---
+++ business/src/containers/CallContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CallContainer extends Component {
+
+  render() {
+    return (
+      <div>Call</div>
+    )
+  }
+}
+
+export default CallContainer;
