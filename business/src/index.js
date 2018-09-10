e8640fe9c28e3c0df94f85123191eaebb339ffa8
Ruth <github email address>
2018-09-10 11:44:10 -0400

    import redux and expand the details of each import

--- business/src/index.js
+++ business/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import manageBusiness from './reducers/manageBusiness'
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(manageBusiness)
+
+ReactDOM.render(
+  <Provider store={store}>
+
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <App />
+  </MuiThemeProvider>
+  </Provider>
+,
+
+  document.getElementById('root')
+);
 registerServiceWorker();
