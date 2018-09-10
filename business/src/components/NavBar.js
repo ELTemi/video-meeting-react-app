652bbe41f9d7cd4918872a36321e25cf6e303024
Ruth <github email address>
2018-09-10 11:42:43 -0400

    design NavBar component to include all navlinks

--- business/src/components/NavBar.js
+++ business/src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '10px',
+  margin: '10px 20px 20px 20px',
+  background: 'grey',
+  textDecoration: 'none',
+  color: 'white',
+
+}
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/">Business</NavLink>
-      <NavLink to="/all_logs">CallLogs</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
-      <NavLink to="/call">Call</NavLink>
+      <NavLink to="/" exact style={link} activeStyle={{background: 'darkgrey'}}>Business</NavLink>
+      <NavLink to="/all_logs" exact style={link} activeStyle={{background: 'darkgrey'}}>CallLogs</NavLink>
+      <NavLink to="/contacts" exact style={link} activeStyle={{background: 'darkgrey'}}>Contacts</NavLink>
+      <NavLink to="/call" exact style={link} activeStyle={{background: 'darkgrey'}}>Video Call Room</NavLink>
     </div>
   );
 };
