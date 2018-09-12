3a4a2d2d34668b7025a51355a5c7e62d9a799df8
Ruth <github email address>
2018-09-12 11:46:10 -0400

    import VideoCall Component to callContainer

--- business/src/containers/CallContainer.js
+++ business/src/containers/CallContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import VideoCall from '../components/VideoCall';
+
 
 class CallContainer extends Component {
 
   render() {
     return (
-      <div>Call</div>
+      <div>
+      <VideoCall />
+      </div>
     )
   }
 }
