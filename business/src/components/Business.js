52321a570944cecc93ead1d1d115ba2af133c9e9
Ruth <github email address>
2018-09-12 11:15:29 -0400

    add new variables to business component

--- business/src/components/Business.js
+++ business/src/components/Business.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
-
 class Business extends Component {
+
+
   render() {
     return (
       <div>
-        <h1>Consult Pearson EnterPrises</h1>
-        <button className="col.25">Create a Room</button>
-        <button className="col.25">Join a Room</button>
+        <h2>Active Meetings</h2>
+        <h2>Contacts</h2>
+        <h2>Completed Meetings</h2>
       </div>
     );
   }
 };
 
 export default Business;
+
+/*axios.get('//localhost:3001/rooms').then(results => {
+console.log(results)
+});*/
