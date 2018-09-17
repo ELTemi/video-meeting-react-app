1b4e30ab2aeebf5c57ee568dc902a52a19f57ce9
Ruth <github email address>
2018-09-17 17:43:50 -0400

    delete contact from store in contacts reducer

--- business/src/reducers/contacts.js
+++ business/src/reducers/contacts.js
@@ -6,7 +6,7 @@ export default function contactsReducer(state = {loading: false, contacts: []}, 
     case 'ADD_CONTACT':
       return { contacts: state.contacts.concat(action.payload) }
     case 'DELETE_CONTACT':
-      const contacts = state.contacts.filter(contact => contact.id !== action.payload.id);
+      const contacts = state.contacts.filter(contact => contact.id !== action.payload);
       return { contacts };
     default:
       return state;
