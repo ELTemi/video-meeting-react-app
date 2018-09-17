b48bc98c7685a9cdcbaf9dd0a91beff7d5f00663
Ruth <github email address>
2018-09-17 16:43:43 -0400

    add delete button to Contact Card component

--- business/src/components/Contact.js
+++ business/src/components/Contact.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import RaisedButton from 'material-ui/RaisedButton';
+import { withStyles } from '@material-ui/core/styles';
 
 
-const ContactCard = ({contact}) =>
-    <TableRow>
-      <TableCell>{contact.name}</TableCell>
-      <TableCell>{contact.phone_number}</TableCell>
-    </TableRow>
+const CustomRaisedButton = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(RaisedButton);
+
+
+class ContactCard extends Component {
+  render(){
+    const { contact } = this.props;
+
+    return (
+      <TableRow>
+        <TableCell>{contact.name}</TableCell>
+        <TableCell>{contact.phone_number}</TableCell>
+        <TableCell><CustomRaisedButton onClick={() => this.props.deleteContact(this.props.contact)}>Delete</CustomRaisedButton></TableCell>
+      </TableRow>
+    )
+  }
+
+
+}
 
 export default ContactCard;
