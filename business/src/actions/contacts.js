171a3c8af5649dcdd2318ea13f5130861113c255
Ruth <github email address>
2018-09-17 16:42:59 -0400

    create addContacts action to actions

--- business/src/actions/contacts.js
+++ business/src/actions/contacts.js
@@ -10,4 +10,24 @@ export function fetchContacts() {
   }
 }
 
+export function addContacts(formData) {
+  return(dispatch) => {
+    dispatch({type: 'POSTING_CONTACTS'});
+    console.log(formData)
+    return fetch('//localhost:3001/contacts', {
+      method: 'POST',
+      body: JSON.stringify({contact: {name: formData.name, phone_number: formData.phoneNumber}}),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        return dispatch({type: 'ADD_CONTACT', payload: responseJSON});
+      })
+  }
+}
+
 export default fetchContacts;
